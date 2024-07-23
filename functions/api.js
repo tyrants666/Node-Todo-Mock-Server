4aafb0f574eb456ec1e369ed24f64096acf9eb1d
tyrants666 <akashyadav.iiita@gmail.com>
2024-07-23 19:05:36 +0545

    Added CORS for API

--- functions/api.js
+++ functions/api.js
@@ -1,3 +1,17 @@
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+};
+
+// Then in your response:
+return {
+  statusCode: 200,
+  headers,
+  body: JSON.stringify(data)
+};
+
+
 const data = require('../db.json')
 
 exports.handler = async (event, context) => {
