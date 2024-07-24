a79fcc5412aede9d77526592086306d4437061f8
tyrants666 <akashyadav.iiita@gmail.com>
2024-07-24 18:16:39 +0545

    Testing / remove CORS error

--- functions/api.js
+++ functions/api.js
@@ -5,13 +5,17 @@ const path = require('path');
 exports.handler = async (event, context) => {
   const headers = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Credentials': 'true'
   };
 
   // Handle preflight requests
   if (event.httpMethod === 'OPTIONS') {
-    return { statusCode: 200, headers };
+    return {
+      statusCode: 204,
+      headers
+    };
   }
 
   const segments = event.path.replace('/api/', '').split('/');
